a0bbbcf908cf20d133306619a951b9819e63c320
yassine.tair <yassine1tair@gmail.com>
2022-08-31 01:07:51 +0100

    create unit test that fails in execution

---
+++ src/main/java/fr/solutions/kata/Diamond.java
@@ -0,0 +1,24 @@
+package fr.solutions.kata;
+
+/**
+ * Class to resolve the famous diamond problem.
+ * @author Yassine Tair
+ *
+ */
+public class Diamond {
+
+	/**
+	 * We pass a character to this method and we should draw the corresponding diamond.
+	 * for example the corresponding diamond to 'C' is:  
+	 *   A
+	 *  B B
+	 * C   C
+	 *  B B
+	 *   A
+	 * @param charactere: must not be null
+	 * @return the diamond we should draw
+	 */
+	public String drawDiamond(char charactere) {
+		return null;
+	}
+}
