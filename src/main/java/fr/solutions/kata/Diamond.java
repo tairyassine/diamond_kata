4f4f06ef5785d1e0c1d85aee241d3d418528614f
yassine.tair <yassine1tair@gmail.com>
2022-08-31 02:29:37 +0100

    Add the logique of the diamond problem and test if the tests passes

--- src/main/java/fr/solutions/kata/Diamond.java
+++ src/main/java/fr/solutions/kata/Diamond.java
@@ -1,5 +1,7 @@
 package fr.solutions.kata;
 
+import static org.junit.Assert.assertNotNull;
+
 /**
  * Class to resolve the famous diamond problem.
  * @author Yassine Tair
@@ -7,6 +9,8 @@ package fr.solutions.kata;
  */
 public class Diamond {
 
+	private static final String END_OF_LINE = "\\n";
+	
 	/**
 	 * We pass a character to this method and we should draw the corresponding diamond.
 	 * for example the corresponding diamond to 'C' is:  
@@ -18,7 +22,66 @@ public class Diamond {
 	 * @param charactere: must not be null
 	 * @return the diamond we should draw
 	 */
-	public String drawDiamond(char charactere) {
-		return null;
-	}
+    public String drawDiamond(Character letter) {
+    	assertNotNull("la lettre passée en parametre ne doit pas etre nulle",letter);
+    	StringBuilder diamond = new StringBuilder();
+        int diamondSize =  letter - 'A' + 1;
+        
+        if (diamondSize == 1) return letter.toString();
+        
+		diamond.append( outsideSpace(diamondSize, 1) + "A" + outsideSpace(diamondSize, 1)).append(END_OF_LINE);
+		        
+		for (int depth = 2; depth < diamondSize * 2 - 1 ; depth ++) {
+	        Character character = Character.toChars('A' + diamondSize - Math.abs(diamondSize - depth) - 1)[0];
+	        StringBuilder builder = new StringBuilder();
+	        String lineAtDepth = builder
+	                .append(outsideSpace(diamondSize, depth)) 
+	                .append(character)
+	                .append(insideSpace(diamondSize, depth))
+	                .append(character.toString())
+	                .append(outsideSpace(diamondSize, depth))
+	                .toString();
+			diamond.append(lineAtDepth);
+            diamond.append(END_OF_LINE);
+        }
+        
+        diamond.append( outsideSpace(diamondSize, 1) + "A" + outsideSpace(diamondSize, 1));
+       
+            
+        return diamond.toString();
+    }
+
+    
+    /**
+     * Calculate and add the required number of the inside spaces for each row.
+     * @param size: the index of the character we would like to add.
+     * @param depth: the depth of the character we would like to add
+     * @return inside spaces to add.
+     */
+    private String insideSpace(int size, int depth) {
+        StringBuilder builder = new StringBuilder();
+        int width = size * 2 - 1;
+        for (int i = 0; i < width - 2 - (2 * Math.abs(size - depth)); i++) {
+            builder.append(" ");
+        }
+        
+        return builder.toString();
+    }
+
+    /**
+     * Calculate and add the required number of the outer spaces for each row.
+     * @param size: the index of the character we would like to add.
+     * @param depth: the depth of the character we would like to add
+     * @return outside spaces to add.
+     */
+    private String outsideSpace(int size, int depth) {
+        StringBuilder builder = new StringBuilder();
+        
+        for (int i = 0; i < Math.abs(size - depth); i++) {
+            builder.append(" ");
+        }
+        
+        return builder.toString();
+    }
+    
 }
--- src/test/java/fr/solutions/kata/DiamondTest.java
+++ src/test/java/fr/solutions/kata/DiamondTest.java
@@ -1,9 +1,6 @@
 package fr.solutions.kata;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
-
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -15,11 +12,11 @@ import org.junit.jupiter.params.provider.CsvSource;
 class DiamondTest {
 
 	@ParameterizedTest
-	@CsvSource({"A,A", "B, A \\nB B\\n A ", "C,  A  \\n B B \\n C   C\\n B B \\n  A  "})
-	void drawDiamondTest(char input, String expected) {
+	@CsvSource({"A,A", "B,A \\nB B\\n A", "C,A  \\n B B \\nC   C\\n B B \\n  A"})
+	void drawDiamondTest(Character input, String expected) {
 		Diamond diamond = new Diamond();
 		String result = diamond.drawDiamond(input);
-		assertEquals(expected, result);
+		assertEquals(expected, result.trim());
 	}
 
 }
