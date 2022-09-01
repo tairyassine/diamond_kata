25f1193a51bddc9a11d8d0eda71e00f9c631570f
yassine.tair <yassine1tair@gmail.com>
2022-09-01 02:17:20 +0100

    refactor code after check of unit test result

--- src/main/java/fr/solutions/kata/Diamond.java
+++ src/main/java/fr/solutions/kata/Diamond.java
@@ -1,6 +1,8 @@
 package fr.solutions.kata;
 
-import static org.junit.Assert.assertNotNull;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Class to resolve the famous diamond problem.
@@ -8,8 +10,10 @@ import static org.junit.Assert.assertNotNull;
  *
  */
 public class Diamond {
-
+	
+	private static final Logger LOGGER = LogManager.getLogger(Diamond.class);
 	private static final String END_OF_LINE = "\\n";
+	private static final String BLANC_SPACE_SEPRATOR = " ";
 	
 	/**
 	 * We pass a character to this method and we should draw the corresponding diamond.
@@ -22,33 +26,101 @@ public class Diamond {
 	 * @param charactere: must not be null
 	 * @return the diamond we should draw
 	 */
-    public String drawDiamond(Character letter) {
-    	assertNotNull("la lettre passée en parametre ne doit pas etre nulle",letter);
-    	StringBuilder diamond = new StringBuilder();
-        int diamondSize =  letter - 'A' + 1;
+    public static String drawDiamond(Character letter) {
+        int diamondSize = diamondSizeForChar(letter);
         
         if (diamondSize == 1) return letter.toString();
+        String diamondResult = buildDiamond(diamondSize);
+        LOGGER.info("diamond of letter [{}] is {}", letter, diamondResult);
+        return diamondResult;
+    }
+
+    /**
+     * Build diamond based on size
+     * @param diamondSize
+     * @return
+     */
+    private static String buildDiamond(int diamondSize) {
+        StringBuilder diamond = new StringBuilder();
         
-		diamond.append( outsideSpace(diamondSize, 1) + "A" + outsideSpace(diamondSize, 1)).append(END_OF_LINE);
-		        
-		for (int depth = 2; depth < diamondSize * 2 - 1 ; depth ++) {
-	        Character character = Character.toChars('A' + diamondSize - Math.abs(diamondSize - depth) - 1)[0];
-	        StringBuilder builder = new StringBuilder();
-	        String lineAtDepth = builder
-	                .append(outsideSpace(diamondSize, depth)) 
-	                .append(character)
-	                .append(insideSpace(diamondSize, depth))
-	                .append(character.toString())
-	                .append(outsideSpace(diamondSize, depth))
-	                .toString();
-			diamond.append(lineAtDepth);
+        diamond.append(drawFirstLine(diamondSize)).append(END_OF_LINE);
+        
+        for (int depth = 2; depth < diamondWidth(diamondSize) ; depth ++) {
+            diamond.append(buildLineAtDepth(diamondSize, depth));
             diamond.append(END_OF_LINE);
         }
         
-        diamond.append( outsideSpace(diamondSize, 1) + "A" + outsideSpace(diamondSize, 1));
-       
-            
+        diamond.append(drawFirstLine(diamondSize));
+        
         return diamond.toString();
+    }
+
+    /**
+     * Calculate the size of the input letter.
+     * @param letter: the input letter
+     * @return size integer
+     */
+    private static int diamondSizeForChar(Character letter) {
+    	int size = letter - 'A' + 1;
+    	LOGGER.info("the size of the given letter [{}] is : [{}]", letter, size);
+        return size;
+    }
+    
+    /**
+     * Draw first line containing 'A' character.
+     * @param diamondSize
+     * @return first line of the diamond
+     */
+    private static String drawFirstLine(int diamondSize) {
+        return addOutsideSpaces(diamondSize, 1) + "A" + addOutsideSpaces(diamondSize, 1);
+    }
+    
+    
+    /**
+     * Draw the other lines based on character entered.
+     * @param diamondSize
+     * @param depth
+     * @return
+     */
+    private static String buildLineAtDepth(int diamondSize, int depth) {
+        Character character = getCharacterAtDepth(diamondSize, depth);
+        StringBuilder builder = new StringBuilder();
+        return builder
+                .append(addOutsideSpaces(diamondSize, depth)) 
+                .append(character)
+                .append(addInsideSpaces(diamondSize, depth))
+                .append(character.toString())
+                .append(addOutsideSpaces(diamondSize, depth))
+                .toString();
+    }
+
+    /**
+     * 
+     * @param diamondSize
+     * @param depth
+     * @return
+     */
+    private static Character getCharacterAtDepth(int diamondSize, int depth) {
+        return Character.toChars('A' + diamondSize - numberOfOutsideSpaces(diamondSize, depth) - 1)[0];
+    }
+
+    /**
+     * 
+     * @param size
+     * @return
+     */
+    private static int diamondWidth(int size) {
+        return size * 2 - 1;
+    }
+
+    /**
+     * 
+     * @param size
+     * @param depth
+     * @return
+     */
+    private static int numberOfOutsideSpaces(int size, int depth) {
+        return Math.abs(size - depth);
     }
 
     
@@ -58,11 +130,11 @@ public class Diamond {
      * @param depth: the depth of the character we would like to add
      * @return inside spaces to add.
      */
-    private String insideSpace(int size, int depth) {
+    private static String addInsideSpaces(int size, int depth) {
         StringBuilder builder = new StringBuilder();
         int width = size * 2 - 1;
-        for (int i = 0; i < width - 2 - (2 * Math.abs(size - depth)); i++) {
-            builder.append(" ");
+        for (int i = 0; i < width - 2 - (2 * numberOfOutsideSpaces(size, depth)); i++) {
+            builder.append(BLANC_SPACE_SEPRATOR);
         }
         
         return builder.toString();
@@ -74,11 +146,11 @@ public class Diamond {
      * @param depth: the depth of the character we would like to add
      * @return outside spaces to add.
      */
-    private String outsideSpace(int size, int depth) {
+    private static String addOutsideSpaces(int size, int depth) {
         StringBuilder builder = new StringBuilder();
         
-        for (int i = 0; i < Math.abs(size - depth); i++) {
-            builder.append(" ");
+        for (int i = 0; i < numberOfOutsideSpaces(size, depth); i++) {
+            builder.append(BLANC_SPACE_SEPRATOR);
         }
         
         return builder.toString();
--- src/test/java/fr/solutions/kata/DiamondTest.java
+++ src/test/java/fr/solutions/kata/DiamondTest.java
@@ -14,8 +14,7 @@ class DiamondTest {
 	@ParameterizedTest
 	@CsvSource({"A,A", "B,A \\nB B\\n A", "C,A  \\n B B \\nC   C\\n B B \\n  A"})
 	void drawDiamondTest(Character input, String expected) {
-		Diamond diamond = new Diamond();
-		String result = diamond.drawDiamond(input);
+		String result = Diamond.drawDiamond(input);
 		assertEquals(expected, result.trim());
 	}
 
